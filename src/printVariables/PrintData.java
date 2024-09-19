f997e3f01b37379b3cf7432dc1bdb9f82824ec00
Soraya2003 <atssoraya@gmail.com>
2024-09-18 20:33:24 -0500

    java v2

---
+++ src/printVariables/PrintData.java
@@ -0,0 +1,71 @@
+package printVariables;
+
+public class PrintData {
+    public static void main(String[] args) {
+        //4 string variable
+        String firstName = "soraya";
+        System.out.println(firstName);
+
+        String lastName = "Ait saidi";
+        System.out.println(lastName);
+
+        String address = "6500 EP TRUE PKWY";
+        System.out.println(address);
+
+        String gender = "female";
+        System.out.println(gender);
+
+        // 4 integer variables
+        int age = 30;
+        System.out.println(age);
+
+        int zipCode = 50266;
+        System.out.println(zipCode);
+
+        int apartmentNumber = 3201;
+        System.out.println(apartmentNumber);
+
+        int pageNumber = 75;
+        System.out.println(pageNumber);
+
+        // 4 char variables
+        char grade = 'A';
+        System.out.println(grade);
+
+        char percentage = '%';
+        System.out.println(percentage);
+
+        char addition = '+';
+        System.out.println(addition);
+
+        char semicolon =';';
+        System.out.println(semicolon);
+
+        //4 double variables
+        double weight = 18.50d;
+        System.out.println(weight);
+
+        double price = 45.99d;
+        System.out.println(price);
+
+        double height = 10.50d;
+        System.out.println(height);
+
+        double volume = 3.5d;System.out.println(volume);
+
+        //4 boolean variables
+        boolean isMarried = true;
+        System.out.println(isMarried);
+
+        boolean isSick = true;
+        System.out.println(isSick);
+
+        boolean isEnrolled = false;
+        System.out.println(isEnrolled);
+
+        boolean hasChildren = false;
+        System.out.println(hasChildren);
+
+
+    }
+}
